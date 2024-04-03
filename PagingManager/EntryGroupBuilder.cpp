d7789e1fb9156edc8136b87b995fa0b1585ea556
atmokam <camilaavakian@gmail.com>
2024-04-03 18:06:35 +0400

    update

--- PagingManager/EntryGroupBuilder.cpp
+++ PagingManager/EntryGroupBuilder.cpp
@@ -34,14 +34,18 @@ namespace PagingManager
 
     void EntryGroupBuilder::buildPageTables(std::shared_ptr<EntryGroup> pdbrEntry)
     {
-        auto currentEntry = pdbrEntry;
-        for(size_t i = 1; i < virtualAddressOffsets.size() - 1; i++)
-        {
-            currentEntry = std::make_shared<EntryGroup>(std::pow(2, virtualAddressOffsets[i]));
-            auto freeIndex = currentEntry->getFirstFreeIndex();
-            auto nextEntry = currentEntry->getIndex(freeIndex);
-            currentEntry = 
-        }
+
+        // we need to somehow walk the page table and if the chain contains no more nullptrs, we need to create a new entry group
+        // but the entries by level in pts won't differ in size
+
+        // auto currentEntry = pdbrEntry;
+        // for(size_t i = 1; i < virtualAddressOffsets.size() - 1; i++)
+        // {
+        //     currentEntry = std::make_shared<EntryGroup>(std::pow(2, virtualAddressOffsets[i]));
+        //     auto freeIndex = currentEntry->getFirstFreeIndex();
+        //     auto nextEntry = currentEntry->getIndex(freeIndex);
+        //     currentEntry = 
+        // }
     }
     
     
