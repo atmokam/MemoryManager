96a64f08ee943405a3f2fb41d07aceb7bd641a8c
atmokam <camilaavakian@gmail.com>
2024-04-08 16:24:02 +0400

    added mapPage functionality

--- MemoryManagementUnit/EntryBaseBuilder.cpp
+++ MemoryManagementUnit/EntryBaseBuilder.cpp
@@ -66,22 +66,7 @@ namespace MMU
     }
 
 
-    // void EntryBaseBuilder::buildPageTablesOn(std::shared_ptr<EntryGroup> pdbrEntry)
-    // {
-
-        // we need to somehow walk the page table and if the chain contains no more nullptrs, we need to create a new entry group
-        // but the entries by level in pts won't differ in size
-
-        // auto currentEntry = pdbrEntry;
-        // for(size_t i = 1; i < virtualAddressOffsets.size() - 1; i++)
-        // {
-        //     currentEntry = std::make_shared<EntryGroup>(std::pow(2, virtualAddressOffsets[i]));
-        //     auto freeIndex = currentEntry->getFirstFreeIndex();
-        //     auto nextEntry = currentEntry->getIndex(freeIndex);
-        //     currentEntry = 
-        // }
-    
-    
+   
     
 }
 // I decided to go with creating the entire pd with nullptr initialization
