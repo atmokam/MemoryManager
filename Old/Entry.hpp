96e9742ce7209bfd8d6ab4a9edbca80cf347f7d0
atmokam <camilaavakian@gmail.com>
2024-04-09 22:12:05 +0400

    completed (partially)

---
+++ Old/Entry.hpp
@@ -0,0 +1,31 @@
+#ifndef ENTRY_HPP
+#define ENTRY_HPP
+
+// Intent: leaf entry (only for PTE in my case)
+
+#include "EntryBase.hpp"
+#include <memory>
+
+namespace MMU
+{
+    class Page;
+    using PageAddress = unsigned int;
+
+    class Entry : public EntryBase
+    {
+        protected:
+            PageAddress& pageAddress;
+            
+        public:
+            Entry(PageAddress& address);
+            Entry() = default;
+
+            bool isValid() const;
+            
+            PageAddress& getPage() const;
+            void setPage(PageAddress& address);
+
+            virtual ~Entry() override = default;
+    };
+}
+#endif
